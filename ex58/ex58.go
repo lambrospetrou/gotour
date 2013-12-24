ffc669e8695bb45f4282ac6429a83070c949ff1b
lambrospetrou <lambrospetrou@gmail.com>
2013-12-24 20:38:11 +0100

    added default handler

--- ex58/ex58.go
+++ ex58/ex58.go
@@ -21,9 +21,17 @@ func (s Struct) ServeHTTP(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w,"%s %s [%s]\n", s.Greeting, s.Punct, s.Who )	
 }
 
+type DefaultHandler struct{}
+func (dh DefaultHandler) ServeHTTP(w http.ResponseWriter, r *http.Request){
+	fmt.Fprintf(w, "Nothing Interesting here!!!\n")
+}
+
+
 func main(){
 	http.Handle( "/string", String("I am a pizza!") )
 	http.Handle( "/struct", &Struct{"Hello",":","Lambros"} )
+	var df DefaultHandler
+	http.Handle( "/", df )
 	http.ListenAndServe("localhost:4000", nil)
 }
 
